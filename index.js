cf2af0b718e864be0be5567556d3656f16d3798e
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-08-31 14:27:52 -0400

    uhh, i'm scared, but I think I added the basic web scraper

---
+++ index.js
@@ -0,0 +1,13 @@
+import puppeteer from 'puppeteer';
+
+const url = "https://www.joshwcomeau.com/";
+
+const main = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({ path: 'screenshot.png'})
+    await browser.close();
+}
+
+main();
