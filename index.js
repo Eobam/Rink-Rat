77825af6598283ead969c33e7b7ab49cde354f43
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-09-01 06:44:24 -0400

    web scraper

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const main = async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    await page.screenshot({ path: 'screenshot.png'})
-    await browser.close();
+
+    
 }
 
 main();
