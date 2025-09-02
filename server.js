024726e1a812b5928f1d19ce158943ffa0b5083f
Elliott Lieberman <elliottlieberman123@gmail.com>
2025-09-02 09:45:07 -0400

    localhost aint working, I think I'm cooked

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+require("dotenv").config();
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",       
-  password: "",       
-  database: "signupDB"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
+
 
 db.connect(err => {
   if (err) throw err;
